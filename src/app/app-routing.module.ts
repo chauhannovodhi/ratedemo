f1e9f88503b135de630f541aee4d0fc017504462
Sanjeev Chauhan <sanjeev.chauhan@novodhi.com>
2021-04-16 02:57:49 +0530

    updaTE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { DemoComponent } from "./demo/demo.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { HomeComponent } from "./home/home.component";
+import { RatingComponent } from "./rating/rating.component";
+import { DialogEditComponent } from "./rating/dialog-edit/dialog-edit.component";
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'rating', component:RatingComponent},
+  {path:'demo', component:DemoComponent},
+  {path:'menu', component:DialogEditComponent},
+  {path:'**',redirectTo: 'pagenotfound' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/rating/dialog-edit/dialog-edit.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {MatMenuModule} from '@angular/material/menu';
+@Component({
+  selector: 'app-dialog-edit',
+  templateUrl: './dialog-edit.component.html',
+  styleUrls: ['./dialog-edit.component.css']
+})
+export class DialogEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  menu:"Hello";
+}
+
+
